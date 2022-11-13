4f632f5c66c44889d50a582e57dc658e97bc5201
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-11-12 23:45:23 -0500

    Actor

---
+++ src/core/actors/actor.gd
@@ -0,0 +1,24 @@
+@tool
+class_name Actor
+extends TileObject
+
+
+@export var cell_offset_direction := Vector2.ZERO:
+	set(value):
+		cell_offset_direction = value
+		_set_offset()
+
+
+@export var cell_offset_distance := 0.0:
+	set(value):
+		cell_offset_distance = value
+		_set_offset()
+
+
+@onready var _offset: Node2D = $Center/Offset
+
+
+func _set_offset() -> void:
+	if _offset:
+		_offset.position = cell_offset_direction.normalized() \
+				* cell_offset_distance * Constants.TILE_SIZE
