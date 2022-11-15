14146e5be71cc15cfdfd23bc2051442b236605c6
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-11-14 21:21:41 -0500

    State machine

---
+++ src/core/game/state/state.gd
@@ -0,0 +1,17 @@
+class_name State
+extends Node
+
+
+signal request_state_change(state_name: String, data: Dictionary)
+
+
+func start(_data: Dictionary) -> void:
+	pass
+
+
+func end() -> void:
+	pass
+
+
+func handle_unhandled_input(_event: InputEvent) -> void:
+	pass
---
+++ src/core/game/state/state_machine.gd
@@ -0,0 +1,28 @@
+class_name StateMachine
+extends Node
+
+@export var initial_state: NodePath
+
+@onready var _current_state: State = get_node(initial_state)
+
+
+func _ready() -> void:
+	for s in get_children():
+		var state := s as State
+		@warning_ignore(return_value_discarded)
+		state.request_state_change.connect(self.change_state)
+	_current_state.start({})
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	_current_state.handle_unhandled_input(event)
+
+
+func change_state(state_name: String, data := {}) -> void:
+	if has_node(state_name):
+		_current_state.end()
+		_current_state = get_node(state_name)
+		_current_state.start(data)
+	else:
+		push_warning("State machine '%s' has no state with name '%s'"
+				% [name, state_name])
