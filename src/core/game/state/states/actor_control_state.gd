6913eb7948b423253e3778f461f5b9e872245f7c
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-11-18 21:19:14 -0500

    Fix player input

--- src/core/game/state/states/actor_control_state.gd
+++ src/core/game/state/states/actor_control_state.gd
@@ -5,12 +5,10 @@ extends GameState
 
 
 func _move_actor(cell: Vector2) -> void:
-	set_process_unhandled_input(false)
 	var path := _game.current_walk_range.get_move_path(
 			_game.current_actor.origin_cell, cell)
 	for p in path:
 		await _game.current_actor.move_step(p)
-	set_process_unhandled_input(true)
 
 
 func _wait() -> void:
--- src/core/game/state/states/player_state.gd
+++ src/core/game/state/states/player_state.gd
@@ -2,13 +2,18 @@ class_name PlayerState
 extends ActorControlState
 
 
+var _blocking_input := false
+
+
 func start(_data: Dictionary) -> void:
-	print("player started")
+	_blocking_input = false
 
 
 func handle_unhandled_input(_event: InputEvent) -> void:
-	if Input.is_action_pressed("click", true):
+	if not _blocking_input and Input.is_action_pressed("click", true):
 		var cell := _game.current_map.mouse_cell
 		if (cell != _game.current_actor.origin_cell) \
 				and _game.current_walk_range.in_move_range(cell):
-			_move_actor(cell)
+			_blocking_input = true
+			await _move_actor(cell)
+			_blocking_input = false
