da74fbdda69c58bbddba067bfe7829b4d366205f
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-11-24 21:02:16 -0500

    Working on choose target mode

--- src/core/game/state/states/player_state.gd
+++ src/core/game/state/states/player_state.gd
@@ -5,7 +5,8 @@ extends ActorControlState
 enum _InnerState
 {
 	CHOOSE_MOVE,
-	ACTION_MENU
+	ACTION_MENU,
+	CHOOSE_TARGET
 }
 
 
@@ -24,6 +25,7 @@ func start(_data: Dictionary) -> void:
 
 
 func end() -> void:
+	assert(not _game.ui.skill_cancelled.is_connected(_start_skill_ended))
 	_game.current_actor.action_menu.attack_selected.disconnect(_attack_selected)
 	_game.current_actor.action_menu.skill_selected.disconnect(_skill_selected)
 	_game.current_actor.action_menu.wait_selected.disconnect(_wait_selected)
@@ -56,11 +58,31 @@ func _action_menu() -> void:
 
 
 func _attack_selected() -> void:
-	await _game.current_actor.close_action_menu()
+	_start_skill_selected(_game.current_actor.attack_skill)
 
 
-func _skill_selected(_skill_index: int) -> void:
+func _skill_selected(skill_index: int) -> void:
+	_start_skill_selected(_game.current_actor.all_skills[skill_index])
+
+
+func _start_skill_selected(skill: Skill) -> void:
 	await _game.current_actor.close_action_menu()
+
+	var targeting_data = skill.get_targeting_data(_game.current_actor)
+
+	_game.map_highlights.clear_all()
+	_game.map_highlights.set_target_range(targeting_data.target_range, targeting_data.valid_targets)
+
+	_game.ui.cancel_skill_visible = true
+	_game.ui.skill_cancelled.connect(
+			_start_skill_ended, ConnectFlags.CONNECT_ONE_SHOT)
+
+	_inner_state = _InnerState.CHOOSE_TARGET
+
+
+func _start_skill_ended() -> void:
+	_game.ui.cancel_skill_visible = false
+	_inner_state = _InnerState.CHOOSE_MOVE
 
 
 func _wait_selected() -> void:
