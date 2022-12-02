d326a81c39635a3014f85865eedd51da50e345a3
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-12-02 14:08:24 -0500

    Got rid of SubViewport

--- src/main.gd
+++ src/main.gd
@@ -1,16 +1,12 @@
 extends Node
 
-@export_range(1, 1, 1, "or_greater") var base_screen_size := 180
-
 @export var initial_map: PackedScene
 
-@onready var _game_viewport: SubViewport = $SubViewportContainer/SubViewport
-@onready var _game: Game = $SubViewportContainer/SubViewport/Game
+@onready var _game: Game = $Game
 
 
 func _ready() -> void:
 	_init_rand()
-	_init_screen()
 	_game.load_map(initial_map)
 
 
@@ -19,18 +15,3 @@ func _init_rand() -> void:
 	seed(seed_number)
 	if OS.is_debug_build():
 		print("Random seed: %d" % seed_number)
-
-
-func _init_screen() -> void:
-	_size_changed()
-	@warning_ignore(return_value_discarded)
-	get_tree().root.size_changed.connect(_size_changed)
-	# Apparently necessary for _size_changed to take effect
-	get_tree().root.size_changed.emit()
-
-
-func _size_changed() -> void:
-	var window_size := get_tree().root.size
-	var min_dimension := mini(window_size.x, window_size.y)
-	var scale := float(base_screen_size) / float(min_dimension)
-	_game_viewport.size_2d_override = Vector2i(window_size * scale)
