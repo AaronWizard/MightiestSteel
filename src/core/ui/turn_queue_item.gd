d4d199f0fa885775fc7d1a89441f667ae251cbca
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-12-06 19:41:24 -0500

    Adding turn queue item and button

---
+++ src/core/ui/turn_queue_item.gd
@@ -0,0 +1,36 @@
+class_name TurnQueueItem
+extends Control
+
+signal clicked
+
+
+var portrait: Texture2D:
+	get:
+		return _portrait.texture
+	set(value):
+		_portrait.texture = value
+
+
+var actor_name: String:
+	get:
+		return _name.text
+	set(value):
+		_name.text = value
+
+
+var is_current: bool:
+	get:
+		return _current_turn_border.visible
+	set(value):
+		_current_turn_border.visible = value
+
+
+@onready var _portrait: TextureRect = $HBoxContainer/Portrait
+@onready var _name: Label = $HBoxContainer/Name
+@onready var _current_turn_border: Control = $CurrentTurnBorder
+
+
+func _gui_input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton) \
+			and (event.button_index == MOUSE_BUTTON_LEFT) and not event.pressed:
+		clicked.emit()
