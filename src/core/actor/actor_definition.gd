bcbd8ad42819d008532bebcf6fe6c94c0b832599
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-12-09 09:56:30 -0500

    Actor stats in CSV files

--- src/core/actor/actor_definition.gd
+++ src/core/actor/actor_definition.gd
@@ -1,17 +1,17 @@
 class_name ActorDefinition
 extends Resource
 
-## An actor's maximum stamina
-@export_range(1, 1, 1, "or_greater") var stamina := 4
+enum _Columns
+{
+	NAME,
+	STAMINA,
+	ATTACK,
+	MOVE,
+	SPEED
+}
 
-## How much damage an actor's attacks do
-@export_range(1, 1, 1, "or_greater") var attack := 1
-
-## The max range in tiles an actor can move as an action
-@export_range(0, 1, 1, "or_greater") var move := 4
-
-## Modifies an actor's turn initiative
-@export_range(0, 1, 1, "or_greater") var speed := 0
+@export_file("*.csv") var stats_file := ""
+@export var stats_name := ""
 
 @export var attack_skill: Skill
 
@@ -20,6 +20,39 @@ extends Resource
 @export var skill1: Skill
 @export var skill2: Skill
 @export var skill3: Skill
+
+
+var stamina: int:
+	get:
+		_check_for_stats()
+		return _stamina
+
+
+var attack: int:
+	get:
+		_check_for_stats()
+		return _attack
+
+
+var move: int:
+	get:
+		_check_for_stats()
+		return _move
+
+
+var speed: int:
+	get:
+		_check_for_stats()
+		return _speed
+
+
+var _stamina: int
+var _attack: int
+var _move: int
+var _speed: int
+
+var _have_stats := false
+
 
 var skills: Array[Skill]:
 	get:
@@ -33,3 +66,32 @@ var skills: Array[Skill]:
 		if skill3:
 			result.append(skill3)
 		return result
+
+
+func _check_for_stats() -> void:
+	if not _have_stats:
+		_load_stats()
+	_have_stats = true
+
+
+func _load_stats() -> void:
+	if stats_file.is_empty():
+		push_error("'%s': stats_file required")
+
+	if stats_name.is_empty():
+		push_error("'%s': stats_name required" % self.resource_path)
+
+	var file := FileAccess.open(stats_file, FileAccess.READ)
+	var found_stats := false
+	while file.get_position() < file.get_length():
+		var row := file.get_csv_line()
+		if row[_Columns.NAME] == stats_name:
+			_stamina = int(row[_Columns.STAMINA])
+			_attack = int(row[_Columns.ATTACK])
+			_move = int(row[_Columns.MOVE])
+			_speed = int(row[_Columns.SPEED])
+			found_stats = true
+			break
+	if not found_stats:
+		push_error("'%s': Could not find row with name '%s' in '%s'."
+				% [self.resource_path, stats_name, stats_file])
