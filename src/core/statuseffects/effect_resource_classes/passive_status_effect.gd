7c5aece29600acdc848fa2cf7d9b1d28d6bbed80
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-12-22 15:21:46 -0500

    Extra doc comment

--- src/core/statuseffects/effect_resource_classes/passive_status_effect.gd
+++ src/core/statuseffects/effect_resource_classes/passive_status_effect.gd
@@ -6,4 +6,5 @@ extends BaseStatusEffect
 ## The effect's icon in an actor's stats panel. Assumed to be 16x16.
 @export var icon: Texture2D
 
+## The effect's description
 @export_multiline var description: String
